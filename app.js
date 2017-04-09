8b67236262a0134a3daea353102b1f9bece88404
GreenLantern101 <arctangent20@yahoo.com>
2017-04-09 11:18:50 -0500

    Indent properly

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var SlackBot = require('slackbots');
-
 var express = require('express'),
     app = express(),
     port = 8000;
@@ -9,28 +7,29 @@ var server = require('http').Server(app);
 //serves all static files in /public
 app.use(express.static(__dirname + '/public'));
 
+//load config file
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync("config.json"));
 
 // create a bot
-
+var SlackBot = require('slackbots');
 var bot = new SlackBot({
-        // Add a bot https://my.slack.com/services/new/bot and put the token
-        token: config.token,
-        name: config.name
-    });
+    // Add a bot https://my.slack.com/services/new/bot and put the token
+    token: config.token,
+    name: config.name
+});
 
-    // more information about additional params https://api.slack.com/methods/chat.postMessage
-    var params = {
-        reply_broadcast: 'false'
-    };
+// more information about additional params https://api.slack.com/methods/chat.postMessage
+var params = {
+    reply_broadcast: 'false'
+};
 
 bot.on('start', function() {
 
-/* view all channels - for debugging
-    var arr = bot.getChannels();
-    console.log(JSON.stringify(arr));
-*/
+    /* view all channels - for debugging
+        var arr = bot.getChannels();
+        console.log(JSON.stringify(arr));
+    */
 
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
     console.log("Bot starting in channel: " + config.channel);
@@ -44,10 +43,12 @@ bot.on('start', function() {
 
 bot.on('message', function(data) {
     // all ingoing events https://api.slack.com/rtm
-    if(data.user && data.text){
-      console.log("In "+ data.channel+", "+data.user+" says: "+data.text);
-      bot.postMessageToChannel(config.channel, 'Message received.', params);
+    if (!data.user && !data.text) {
+        return;
     }
+    console.log("In " + data.channel + ", " + data.user + " says: " + data.text);
+    bot.postMessageToChannel(config.channel, 'Message received.', params);
+
 });
 
 
