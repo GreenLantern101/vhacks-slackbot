86e8e0b90c5f5832069aece2c908a31cb45932e8
GreenLantern101 <arctangent20@yahoo.com>
2017-04-08 21:49:54 -0500

    .

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 var SlackBot = require('slackbots');
 
+var express = require('express'),
+    app = express(),
+    port = 8000;
+
+var server = require('http').Server(app);
+
+//serves all static files in /public
+app.use(express.static(__dirname + '/public'));
+
 // create a bot
+
 var bot = new SlackBot({
-    token: '<token here>', // Add a bot https://my.slack.com/services/new/bot and put the token
-    name: 'VandyHacks Slackbot'
-});
+        // Add a bot https://my.slack.com/services/new/bot and put the token
+        token: '',
+        name: 'VandyHacks Slackbot'
+    });
 
 bot.on('start', function() {
     // more information about additional params https://api.slack.com/methods/chat.postMessage
     var params = {
-        icon_emoji: ':cat:'
+        reply_broadcast: 'false'
     };
 
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services
-    bot.postMessageToChannel('general', 'meow!', params){
-
-    }
+    bot.postMessageToChannel('general', 'Hello!', params);
 
     // define existing username instead of 'user_name'
-    bot.postMessageToUser('user_name', 'meow!', params){
+    bot.postMessageToUser('user_name', 'Hello!', params);
 
-    }
+});
 
+bot.on('message', function(data) {
+    // all ingoing events https://api.slack.com/rtm
+    console.log(data);
+});
+
+
+server.listen(port, function() {
+    console.log('Listening on port ' + port)
 });
