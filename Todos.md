17f11c910f387b52df28a21af265e18667b00fe7
GreenLantern101 <arctangent20@yahoo.com>
2017-04-09 11:59:22 -0500

    Create minor app structure

---
+++ Todos.md
@@ -0,0 +1,5 @@
+# Todos:
+
+1. Have user search functionality/tags(from keywords), mark read/unread...
+2. Secure MongoDB instance for running in production
+3.
---
+++ public/dashboard.js
@@ -0,0 +1,35 @@
+Vue.use(VueMaterial);
+
+var app = new Vue({
+    el: '#mainfeed',
+    data: {
+        messages: []
+    },
+    methods: {
+
+    }
+});
+
+
+function getJSON(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status);
+        }
+    };
+    xhr.send();
+};
+
+function refresh() {
+    getJSON('data/messagefeed', function(err, res) {
+        //console.log(app.news);
+        app.messages = res.messages;
+    });
+}
+refreshnews();
