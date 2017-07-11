4855aea3dbd67a017617c1f65ef0f26bc862330b
GreenLantern101 <arctangent20@yahoo.com>
2017-07-11 16:26:01 -0500

    Use promises

--- app.js
+++ app.js
@@ -21,66 +21,83 @@ bot.on('start', () => {
   console.log(`Bot starting in channel: ${process.env.channel}`);
 });
 
-// on posted message
-bot.on('message', (data) => {
-    // only handle valid messages directed at the bot
-  if (!data.user || !data.text || !data.channel) {
+
+const processMessage = (userObj, channelObj, data) => {
+
+  if (!channelObj) {
+    console.log(`Channel ${data.channel} can't be found in listed channels.`);
+    return;
+  }
+  if (!userObj) {
+    console.log(`User ${data.user} can't be found in listed users.`);
     return;
   }
 
-    // require direct mention if not DMed
+  // require direct mention if not DMed
+  // only handle valid messages directed at the bot
+  /*
   if (!data.text.includes('<@U4VTCUZ6U>')) {
     console.log(data);
     return;
   }
+  */
 
-    // remove mention text
+  // remove mention text
   const feedback = data.text.replace(/<@U4VTCUZ6U>/g, '').trim();
 
-    // extract user and channel
-  const user = find(bot.getUsers()._value.members, 'id', data.user);
-  const channel = find(bot.getChannels()._value.channels, 'id', data.channel);
+  // log feedback in console
+  console.log("In " + channel.name + ", " + user.name + " says: " + data.text);
 
-  if (!channel) {
-    console.log("channel can't be found");
-    console.log(bot.getChannels());
-    // return;
-  }
-
-    // log feedback in console
-    // console.log("In " + channel.name + ", " + user.name + " says: " + data.text);
-
-    // thank user in channel for feedback in the same channel it was submitted in
-  bot.postMessageToChannel(data.channel,
+  // thank user for feedback in the same channel it was submitted in
+  bot.postMessage(data.channel,
     `Thanks for your feedback, ${user.name}!`,
     params);
 
-    /* fast check to see if invalid input
-    * invalid if <6 alphabetic chars
-    */
+  /* fast check to see if invalid input
+   * invalid if <6 alphabetic chars
+   */
   if (feedback.replace(/[^A-Za-z]+/g, '').length < 6) {
     // if invalid, don't forward or save to db
     return;
   }
 
-    // forward feedback to private group
+  // forward feedback to private group
   bot.postMessageToGroup(process.env.privatechannel, `${'Feedback submitted:\n' +
     'Text: '}${feedback}\n` +
     `TimeStamp: ${data.ts}\n` +
     `Channel_ID (anonymous): ${data.channel}\n` +
     `User_ID (anonymous): ${data.user}`);
 
-    // console.log(data);
+  // console.log(data);
 
-    // TODO: add message to PostgreSQL db
-});
+  // TODO: add message to PostgreSQL db
+};
 
-// returns 1st value that matches
-function find(arr, field, value) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i][field] === value) {
-      return arr[i];
-    }
+const findUser = (userID) =>
+  bot.getUsers()
+  .then(obj => obj.members.filter(user => user.id == userID))
+  .then(arr => console.log(arr))
+  .catch(err => console.log(err));
+
+const findChannel = (channelID) =>
+  bot.getChannels()
+  .then(obj => obj.channels.filter(channel => channel.id == channelID))
+  .then(arr => console.log(arr))
+  .catch(err => console.log(err));
+
+// on posted message
+bot.on('message', (data) => {
+
+  // critical message data missing
+  if (!data.user || !data.text || !data.channel) {
+    console.log("ERROR: part of data missing from this message. Ignoring message.");
+    console.log(data);
+    return;
   }
-  return undefined;
-}
+
+  console.log(data);
+
+  // find user and channel objects & process message
+  Promise.all([findUser(data.user), findChannel(data.channel)])
+    .then(([userObj, channelObj]) => processMessage(userObj, channelObj, data));
+});
